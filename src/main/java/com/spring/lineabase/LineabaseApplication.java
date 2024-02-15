b350de9c2ebcdb84ad62a3a9e43d1af7d4da38d7
Daniel Henao <dhenao693@gmail.com>
2024-02-15 09:08:48 -0500

    iniciar proyecto

---
+++ src/main/java/com/spring/lineabase/LineabaseApplication.java
@@ -0,0 +1,13 @@
+package com.spring.lineabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/lineabase/LineabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lineabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
